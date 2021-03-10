2c5dde3dbc0f50211fde21e8445951a0ee35ed7a
Rajput Kapilesh <rajputkapilesh@outlook.com>
2021-03-10 22:50:48 +0530

    Added some mock data to ViewModel

--- app/src/main/java/com/example/kapilesh/flags/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/kapilesh/flags/viewmodel/ListViewModel.kt
@@ -1,6 +1,38 @@
 package com.example.kapilesh.flags.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kapilesh.flags.Country
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("Country A"),
+            Country("Country B"),
+            Country("Country C"),
+            Country("Country D"),
+            Country("Country E"),
+            Country("Country F"),
+            Country("Country G"),
+            Country("Country H"),
+            Country("Country I"),
+            Country("Country J"),
+            Country("Country K")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+
 }
